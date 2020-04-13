6baf86cfecd97c8d90eab76088b5c31766fd5ca8
Mitul Sheth <mitul.sheth@tamr.com>
2020-04-12 22:45:30 -0400

    Joins and coalesce explanations

--- src/main/java/Joins.java
+++ src/main/java/Joins.java
@@ -1,2 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.Optional;
+
+import scala.Tuple2;
+
 public class Joins {
+    public static void main(String[] args) {
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+        final SparkConf conf = new SparkConf().setAppName("Starting Spark").setMaster("local[*]");
+        final JavaSparkContext sc = new JavaSparkContext(conf);
+
+        final List<Tuple2<Integer, Integer>> leftTable = new ArrayList<>();
+        leftTable.add(new Tuple2<>(1, 18));
+        leftTable.add(new Tuple2<>(2, 4));
+        leftTable.add(new Tuple2<>(10, 9));
+
+        final List<Tuple2<Integer, String>> rightTable = new ArrayList<>();
+        rightTable.add(new Tuple2<>(1, "Mitul"));
+        rightTable.add(new Tuple2<>(2, "Zarna"));
+        rightTable.add(new Tuple2<>(3, "Parin"));
+        rightTable.add(new Tuple2<>(4, "Chirag"));
+        rightTable.add(new Tuple2<>(5, "Sagar"));
+        rightTable.add(new Tuple2<>(6, "Sahil"));
+
+        final JavaPairRDD<Integer, Integer> leftRDD = sc.parallelizePairs(leftTable);
+        final JavaPairRDD<Integer, String> rightRDD = sc.parallelizePairs(rightTable);
+
+        // inner join
+        System.out.println("Inner join");
+        final JavaPairRDD<Integer, Tuple2<Integer, String>> joinRDD = leftRDD.join(rightRDD);
+        joinRDD.collect().forEach(System.out::println);
+        System.out.println();
+
+        // left join
+        System.out.println("Left join");
+        final JavaPairRDD<Integer, Tuple2<Integer, Optional<String>>> leftJoinRDD =
+          leftRDD.leftOuterJoin(rightRDD);
+        leftJoinRDD.collect().forEach(System.out::println);
+        System.out.println();
+
+        // right join
+        System.out.println("Right join");
+        final JavaPairRDD<Integer, Tuple2<Optional<Integer>, String>> rightJoinRDD =
+          leftRDD.rightOuterJoin(rightRDD);
+        rightJoinRDD.collect().forEach(System.out::println);
+        System.out.println();
+
+        // full join
+        System.out.println("Full outer join");
+        final JavaPairRDD<Integer, Tuple2<Optional<Integer>, Optional<String>>> fullJoinRDD =
+          leftRDD.fullOuterJoin(rightRDD);
+        fullJoinRDD.collect().forEach(System.out::println);
+        System.out.println();
+
+        // cross join
+        System.out.println("Cross join");
+        final JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Integer, String>> crossJoinRDD =
+          leftRDD.cartesian(rightRDD);
+        crossJoinRDD.collect().forEach(System.out::println);
+        System.out.println();
+
+        sc.close();
+    }
 }
--- src/main/java/ReadingFromFile.java
+++ src/main/java/ReadingFromFile.java
@@ -30,11 +30,34 @@ public class ReadingFromFile {
       .take(10)
       .forEach(System.out::println);
     // doing foreach here directly after sortByKey won't work here, it does not give the desired
-    // output
-    // one would believe that the foreach is executed onto each partition and then returned
-    // So, one might believe that coalescing all partitions on to one partition can solve the
-    // problem.
-    // Not true, coalescing onto one partition can result in out of memory error.
+    // output. One would believe that the foreach is executed onto each partition and then returned.
+    //
+    // Guessing it intuitively, the first thing that comes to mind is that this can be achieved if
+    // we have collect() instead of take(n). But the results will not be sorted as expected.
+    // Rather there  will be chunk partitions which are sorted.
+    // Okay. This means that this is because the results are obtained asynchronously from different
+    // partitions, that the sorting is done on each partition separately and then outputted.
+    // So, one might believe that coalescing all partitions on to one partition, using
+    // coealesce(1) can solve the problem. And it kind of does too.
+    //
+    // UTTER BULLSHIT AND WRONG. Most texts, courses and people will say this is the reason.
+    // There are so many things wrong here.
+    //
+    // First, any such coalesce operations will bring data from all partitions, (which in
+    // production environment will be on different nodes) onto a single partition (node) and this
+    // can cause an out of memory error.
+    // Second, in real production environments, sending printing will actually print on that
+    // particular node and we won't see the output appear here. Locally, yes there are different
+    // threads but on the same JVM. So, we can see the output even though distorted.
+    // Third, correctness of a business logic should not be dependent on the number of partitions
+    // of Spark, or any performance considerations for that matter. You do not need to shuffle
+    // data or need to know the number of partitions for getting the right answer.
+    //
+    // So, the correct thing to do for correctness is use operations like take or collect. Here,
+    // the emphasis is on correctness and not on the performance. Use collect only if you are
+    // confident that a single JVM can take that much data.
+    // Also, coalesce is used just to reduce the number of partitions when it does not make sense
+    // to have an entire partition for a small amount of data.
     sc.close();
   }
 }
